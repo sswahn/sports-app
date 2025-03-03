162ef453e2dfc90c924098a15e63dda8685c4dab
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 12:16:54 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,7 @@
-import { useContext } from 'react'
-import { Context } from '../../Provider'
-import BarsIcon from '../Icons/BarsIcon/BarsIcon'
 
 const Header = () => {
-  const [context, dispatch] = useContext(Context)
-  
-  const handleSidebar = event => {
-    dispatch({ type: 'sidebar', payload: true })
-  }
-
   return (
     <header className="header">
-      <button className="sidebar-btn" onClick={handleSidebar} type="button" aria-label="open menu" aria-controls="sidebar" aria-expanded={context.sidebar}>
-        <BarsIcon />
-        <div className="tooltip" role="tooltip">Menu</div>
-      </button>
     </header>
   )
 }
