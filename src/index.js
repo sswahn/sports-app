cedabf2c42c0346cd238085d7852763819022410
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 10:52:08 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//import * as serviceWorker from './serviceWorker'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(<App />)
+
+//serviceWorker.register()
