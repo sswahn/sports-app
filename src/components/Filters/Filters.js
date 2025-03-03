7695d6f62fb6a228a492c136d171ff90d6d6389d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 16:56:30 -0500

    Create Filters.js

---
+++ src/components/Filters/Filters.js
@@ -0,0 +1,14 @@
+
+
+const Filters = () => {
+  return (
+    <select>
+      <option value="">View All Ages</option>
+      <option value="saab">U7 - U9</option>
+      <option value="opel">U11 - U13</option>
+      <option value="audi">U13 and up</option>
+    </select>
+  )
+}
+
+export default Filters
