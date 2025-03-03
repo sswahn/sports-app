edefcee0f0a41407c1a044dfef571288d1610dac
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 12:58:18 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(process.cwd(), 'build'),
-    publicPath: '/devour-clone/',
+    publicPath: '/sports-app/',
   },
     
   module: {
