068f4e798de509df6dd0499c0f49102d10a898d6
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 15:26:17 -0500

    Update DrillList.js

--- src/components/DrillList/DrillList.js
+++ src/components/DrillList/DrillList.js
@@ -14,7 +14,31 @@ const DrillList = () => {
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg" alt="Checking Skills - 1 Up - 1 Down - Gap Drill" />
       </div>
+      <div>
+        <header>
+          <h2>Checking Skills - Continuous Angling</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_continuous_angling.jpg" alt="Checking Skills - Continuous Angling" />
+      </div>
+      <div>
+        <header>
+          <h2>Checking Skills - Forecheck Progression</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression.jpg" alt="Checking Skills - Forecheck Progression" />
+      </div>
 
+      <div>
+        <header>
+          <h2>Checking Skills - Forecheck Progression - F1 - F2</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression_F1_F2.jpg" alt="Checking Skills - Forecheck Progression - F1 - F2" />
+      </div>
+      <div>
+        <header>
+          <h2>Checking Skills - 1 Up - 1 Down - Gap Drill</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg" alt="Checking Skills - 1 Up - 1 Down - Gap Drill" />
+      </div>
       <div>
         <header>
           <h2>Checking Skills - Continuous Angling</h2>
