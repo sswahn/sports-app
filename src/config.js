9bf83e8c50aa98f9927795797d477ece4c1f1728
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 11:37:57 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -28,6 +28,15 @@ export const config = {
         'Small Area Game',
         'Warm-up Drill',
         'Goaltending Drill'
+      ],
+      sort: [
+        'Alphabetical',
+        'Date Added'
+      ],
+      count: [
+        12,
+        24,
+        48
       ]
     }
   }
