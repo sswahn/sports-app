8f72fe71e8fceea69ea117f4e428c995a7b27743
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 18:35:18 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ErrorBoundary from './ErrorBoundary'
 import Provider from './Provider'
 import Header from './components/Header/Header'
 import Drills from './components/Drills/Drills'
+import Footer from './components/Footer/Footer'
 import './index.css'
 
 export default () =>
@@ -10,6 +11,7 @@ export default () =>
       <Provider>
         <Header />
         <Drills />
+        <Footer />
       </Provider> 
     </ErrorBoundary> 
   </React.StrictMode>
