a44925a53464f3d38a0533bed7e1e5968d152b08
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 19:48:58 -0500

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -7,7 +7,7 @@ const Filters = () => {
   
   return (
     <select onChange={handleAges}>
-      <option disabled>View All Ages</option>
+      <option hidden>View All Ages</option>
       <option>U7 - U9</option>
       <option>U11 - U13</option>
       <option>U13 and up</option>
