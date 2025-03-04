5387bc9bfe27a2a1f06d8f223d75a43bd6321095
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 22:12:20 -0500

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,5 +1,29 @@
 import Select from '../Select/Select'
 
+const ages = [
+  'View All Ages', 
+  'U7 - U9', 
+  'U11 - U13', 
+  'U13 and up'
+]
+
+const categories = [
+  'View All Categories', 
+  'Skating', 
+  'Defense', 
+  'Puck Control', 
+  'Shooting', 
+  'Scoring',
+  'Passing',
+  'Individual Skill',
+  'Individual Tactic',
+  'Team Tactic',
+  'Checking Drill',
+  'Small Area Game',
+  'Warm-up Drill',
+  'Goaltending Drill'
+]
+
 const Filters = () => {
 
   const handleAges = event => {
@@ -7,30 +31,10 @@ const Filters = () => {
   }
   
   return (
-    <>
-    <Select options={[
-      'View All Ages', 
-      'U7 - U9', 
-      'U11 - U13', 
-      'U13 and up'
-    ]} onChange={handleAges} />
-    <Select options={[
-      'View All Categories', 
-      'Skating', 
-      'Defense', 
-      'Puck Control', 
-      'Shooting', 
-      'Scoring',
-      'Passing',
-      'Individual Skill',
-      'Individual Tactic',
-      'Team Tactic',
-      'Checking Drill',
-      'Small Area Game',
-      'Warm-up Drill',
-      'Goaltending Drill'
-    ]} onChange={handleAges} />
-    </>
+    <div>
+      <Select options={ages} onChange={handleAges} />
+      <Select options={categories} onChange={handleAges} />
+    </div>
   )
 }
 
