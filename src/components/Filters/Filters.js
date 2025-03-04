4b2fabd7181692f3210edb587552a38ef0b35e4d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 04:52:05 -0500

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,41 +1,26 @@
 import Select from '../Select/Select'
 import Search from '../Search/Search'
-
-const ages = [
-  'View All Ages', 
-  'U7 - U9', 
-  'U11 - U13', 
-  'U13 and up'
-]
-
-const categories = [
-  'View All Categories', 
-  'Skating', 
-  'Defense', 
-  'Puck Control', 
-  'Shooting', 
-  'Scoring',
-  'Passing',
-  'Individual Skill',
-  'Individual Tactic',
-  'Team Tactic',
-  'Checking Drill',
-  'Small Area Game',
-  'Warm-up Drill',
-  'Goaltending Drill'
-]
+import { config } from '../../config'
 
 const Filters = () => {
 
   const handleAges = event => {
     console.log('event.target.value: ', event.target.value)
   }
+
+  const handleCategories = event => {
+    console.log('event.target.value: ', event.target.value)
+  }
+
+  const handleSearch = event => {
+    console.log('event.target.value: ', event.target.value)
+  }
   
   return (
     <div>
-      <Select options={ages} onChange={handleAges} />
-      <Select options={categories} onChange={handleAges} />
-      <Search ckassName="search-drills" placeholder="Search Srills" />
+      <Select options={config.data.drills.ages} onChange={handleAges} />
+      <Select options={config.data.drills.categories} onChange={handleCategories} />
+      <Search ckassName="search-drills" placeholder="Search Srills" onSubmit={handleSearch} />
     </div>
   )
 }
