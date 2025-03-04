c66ea2c6b14ac5142b3661a65c01cb2b448baae9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 10:08:41 -0500

    Update DrillList.js

--- src/components/DrillList/DrillList.js
+++ src/components/DrillList/DrillList.js
@@ -1,7 +1,12 @@
 
 const DrillList = () => {
   return (
-    <div></div>
+    <div>
+      <div>
+        <h2>Goaltending - Alternating Nets</h2>
+        <img src="/images/goaltending_alternating_nets.jpg" alt="Goaltending -Alternating Nets" />
+      </div>
+    </div>
   )
 }
 
