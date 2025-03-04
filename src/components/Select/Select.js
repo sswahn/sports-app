747217c3afbe199f481c2ce738d5c1ff2c276bae
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 21:51:14 -0500

    Update Select.js

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -2,7 +2,7 @@
 const Select = ({ options, onChange }) => {
   return (
     <select className="select" onChange={onChange}>
-      {options.map((item, index) => <option key={index} {index === 0 ? 'hidden' : ''}>{item}</option>)}
+      {options.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
     </select>
   )
 }
