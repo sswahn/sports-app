6b302686ae50f7a543184113b8db430b88e0c232
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-03 19:45:53 -0500

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,16 +1,13 @@
-import { useState } from 'react'
 
 const Filters = () => {
 
-  const [ages, setAges] = useState('View All Ages')
-
   const handleAges = event => {
     console.log('event.target.value: ', event.target.value)
-    setAges(event.target.value)
   }
   
   return (
-    <select value={ages} onChange={handleAges}>
+    <select onChange={handleAges}>
+      <option disabled>View All Ages</option>
       <option>U7 - U9</option>
       <option>U11 - U13</option>
       <option>U13 and up</option>
