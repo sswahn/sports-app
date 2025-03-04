bb8ffea139f98fe5daae08c819518cff11c4a8e9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-04 14:57:52 -0500

    Update DrillList.js

--- src/components/DrillList/DrillList.js
+++ src/components/DrillList/DrillList.js
@@ -14,6 +14,19 @@ const DrillList = () => {
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg" alt="Checking Skills - 1 Up - 1 Down - Gap Drill" />
       </div>
+
+      <div>
+        <header>
+          <h2>Goaltending - Alternating Nets</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/goaltending_alternating_nets.jpg" alt="Goaltending - Alternating Nets" />
+      </div>
+      <div>
+        <header>
+          <h2>Checking Skills - 1 Up - 1 Down - Gap Drill</h2>
+        </header>
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg" alt="Checking Skills - 1 Up - 1 Down - Gap Drill" />
+      </div>
     </div>
   )
 }
