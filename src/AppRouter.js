33b584de20b23cecf23ccf9be755ab6c87d48b4f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-05 08:57:17 -0500

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,12 +2,14 @@ import { lazy } from 'react'
 import { Router, Route } from '@sswahn/router'
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner'
 import Drills from './components/Drills/Drills'
+const CreateDrill = lazy(() => import('./components/CreateDrill/CreateDrill'))
 const NotFound = lazy(() => import('./components/NotFound/NotFound'))
 
 const AppRouter = () => {
   return (
     <Router lazyFallback={LoadingSpinner}>
       <Route path="/sports-app/" component={Drills} />
+      <Route path="/sports-app/create-drill" component={CreateDrill} />
       <Route component={NotFound} /> 
     </Router>
   )
