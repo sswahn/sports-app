6d314acd73d7c7ea269063ac71012b7cd6d4bbcc
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-05 17:08:59 -0500

    Update Provider.js

--- src/Provider.js
+++ src/Provider.js
@@ -6,11 +6,14 @@ export const Context = createContext([])
 export default function Provider({ children }) {
   const data = {
     modal: undefined,
+    drill: undefined
   }
   const reducer = (state, action) => {
     switch(action.type) {
       case 'modal':
         return { ...state, modal: action.payload }
+      case 'drill':
+        return { ...state, drill: action.payload }
       default:
         return state
     }
