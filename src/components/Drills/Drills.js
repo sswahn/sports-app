432ba5ae7be080bf8a0e22245e386de52011b4b3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-05 12:35:05 -0500

    Update Drills.js

--- src/components/Drills/Drills.js
+++ src/components/Drills/Drills.js
@@ -1,9 +1,16 @@
 import Filters from '../Filters/Filters'
 import DrillList from '../DrillList/DrillList'
+import navigateTo from '../Router/navigateTo'
 
 const Drills = () => {
+
+  const handleCreateDrill = event => {
+    navigateTo('/create-drill')
+  }
+  
   return (
     <div className="drills">
+      <button onClick={handleCreateDrill}>Create</button>
       <Filters />
       <DrillList />
     </div>
