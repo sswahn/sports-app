ca567770749cb4b1a59a5fa9c60762f482c386f0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-05 17:34:58 -0500

    Update DrillList.js

--- src/components/DrillList/DrillList.js
+++ src/components/DrillList/DrillList.js
@@ -1,75 +1,84 @@
+import { useContext } from 'react'
+import { Context } from '../../Provider'
+
 
 const DrillList = () => {
+  const [context, dispatch] = useContext(Context)
+
+  const handleDrill = event => {
+    dispatch({ type: 'drill', payload: event.target.id })
+  }
+  
   return (
     <div className="drill-list">
-      <div>
+      <div id="1" onClick={handleDrill}>
         <header>
           <h2>Goaltending - Alternating Nets</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/goaltending_alternating_nets.jpg" alt="Goaltending - Alternating Nets" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - 1 Up - 1 Down - Gap Drill</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg" alt="Checking Skills - 1 Up - 1 Down - Gap Drill" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Continuous Angling</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_continuous_angling.jpg" alt="Checking Skills - Continuous Angling" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Forecheck Progression</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression.jpg" alt="Checking Skills - Forecheck Progression" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Forecheck Progression - F1 - F2</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression_F1_F2.jpg" alt="Checking Skills - Forecheck Progression - F1 - F2" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Long Angle</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_long_angle.jpg" alt="Checking Skills - Long Angle" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Mirror Gap Control</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_mirror_gap_control.jpg" alt="Checking Skills - Mirror Gap Control" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Defense - Retrieval - Attack F1</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/defence_retreival_attack_F1.jpg" alt="Defense - Retrieval - Attack F1" />
       </div>
 
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Forecheck Progression - F1 - F2</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression_F1_F2.jpg" alt="Checking Skills - Forecheck Progression - F1 - F2" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Long Angle</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_long_angle.jpg" alt="Checking Skills - Long Angle" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Checking Skills - Mirror Gap Control</h2>
         </header>
         <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_mirror_gap_control.jpg" alt="Checking Skills - Mirror Gap Control" />
       </div>
-      <div>
+      <div onClick={handleDrill}>
         <header>
           <h2>Defense - Retrieval - Attack F1</h2>
         </header>
