5300807fac68c760cc810356cdcef6854b8219f9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-06 09:56:33 -0500

    Update Drill.js

--- src/components/Drill/Drill.js
+++ src/components/Drill/Drill.js
@@ -8,6 +8,8 @@ const Drill = ({ data }) => {
       <h2>{data.title}</h2>
       <img src={data.image} alt={data.title} />
       <p>{data.description}</p>
+      <strong>Key Teaching Points</strong>
+      <p>{data.subdescription}</p>
       <p>{data.age}</p>
       <p>{data.category}</p>
     </article>
