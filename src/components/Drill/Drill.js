329d94cd3866ca310e8a7af33bae0f7d43a0e717
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-06 15:19:34 -0500

    Update Drill.js

--- src/components/Drill/Drill.js
+++ src/components/Drill/Drill.js
@@ -2,13 +2,17 @@
 const Drill = ({ data }) => {
   return (
     <article className="drill">
-      <h2>{data.title}</h2>
-      <img src={data.image} alt={data.title} />
+      <header>
+        <h2>{data.title}</h2>
+        <img src={data.image} alt={data.title} />
+      </header>
       <p>{data.description}</p>
       <strong>Key Teaching Points:</strong>
       <p>{data.subdescription}</p>
-      <small><b>Age</b>: {data.age}</small> &nbsp; 
-      <small><b>Category</b>: {data.category}</small>
+      <footer>
+        <small><b>Age</b>: {data.age}</small> &nbsp; 
+        <small><b>Category</b>: {data.category}</small>
+      </footer>
     </article>
   )
 }
