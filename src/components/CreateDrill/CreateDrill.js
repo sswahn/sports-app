197204521ae6047f372342b1f85e32e511e9417e
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-06 04:45:47 -0500

    Update CreateDrill.js

--- src/components/CreateDrill/CreateDrill.js
+++ src/components/CreateDrill/CreateDrill.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { Context } from '../../Provider'
 import Select from '../Select/Select'
 import { config } from '../../config'
 
 const CreateDrill = () => {
+  const [context, dispatch] = useContext(Context)
   const [preview, setPreview] = useState(undefined)
     
   const handleAges = event => {
@@ -25,26 +27,32 @@ const CreateDrill = () => {
     
   const handlePreview = event => {
     console.log('open preview modal.')
+    dispatch({ type: 'preview', payload: true })
   }
   
   return (
-    <form className="create-drill">
+    <div>
+      <form className="create-drill">
       <h2>Create A Drill</h2>
-      <input tyoe="text" placeholder="Title of drill" />
-      <textarea placeholder="Description of drill."></textarea>
-      <div>
-        <input type="file" accept="image/*" onChange={handleImage} />
-        {preview && <img className="drill-image-preview" src={preview} alt="Preview" />}
-      </div>
-      <div>
-        <Select options={config.data.drills.ages.map((val, i) => (i === 0 ? 'Choose An Age Group' : val))} onChange={handleAges} />
-        <Select options={config.data.drills.categories.map((val, i) => (i === 0 ? 'Choose A Category' : val))} onChange={handleCategories} />
-      </div>
-      <div>
-        <button onClick={handlePreview}>Preview</button>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+        <input tyoe="text" placeholder="Title of drill" />
+        <textarea placeholder="Description of drill."></textarea>
+        <div>
+          <input type="file" accept="image/*" onChange={handleImage} />
+          {preview && <img className="drill-image-preview" src={preview} alt="Preview" />}
+        </div>
+        <div>
+          <Select options={config.data.drills.ages.map((val, i) => (i === 0 ? 'Choose An Age Group' : val))} onChange={handleAges} />
+          <Select options={config.data.drills.categories.map((val, i) => (i === 0 ? 'Choose A Category' : val))} onChange={handleCategories} />
+        </div>
+        <div>
+          <button onClick={handlePreview}>Preview</button>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <Modal className="modal" open={context.drill} onClose={handleCloseDrill}>
+        {}
+      </Modal>
+  </div>
   )
 }
 
