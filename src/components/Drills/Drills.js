ad07bfaa27f521d2d7efcdf83d7f0b6acf8d5441
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-06 16:37:50 -0500

    Update Drills.js

--- src/components/Drills/Drills.js
+++ src/components/Drills/Drills.js
@@ -24,7 +24,7 @@ const Drills = () => {
   
   return (
     <div className="drills">
-      <button className="primary" onClick={handleCreateDrill}>Create</button>
+      <button className="create-btn" onClick={handleCreateDrill}>Create</button>
       <Filters />
       <DrillList />
       <Modal className="modal" open={context.drill} onClose={handleCloseDrill}>
