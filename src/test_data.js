d5988b154c0121acd067bad3306c9a7e35735b19
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 09:49:59 -0500

    Create test_data.js

---
+++ src/test_data.js
@@ -0,0 +1,16 @@
+export const data = {
+  drills: [
+    {
+      title: 'Goaltending - Alternating Nets',
+      description: 'Place 2 nets as shown, facing each other but not in front of each other so they are offset. Inside posts can be aligned. Goaltender starts in one net gets 2 shots, then has to turn and butterfly slide to get in the other net and then face 2 shots again. Do the sequence twice then rest',
+      subdescription: 'Goaltender has to be set, quick turn, then slide to other net. Shooters let goalie get in position before shooting.',
+      image: '',
+      age: 'U7 - U9',
+      category: 'Goaltending Drill',
+      date: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()
+    },
+    {
+      
+    }
+  ]
+}
