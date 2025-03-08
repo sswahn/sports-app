a2cae1986e0a780057229d4742f3c6a6e34cb01c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 12:31:49 -0500

    Update DrillList.js

--- src/components/DrillList/DrillList.js
+++ src/components/DrillList/DrillList.js
@@ -3,7 +3,7 @@ const DrillList = ({ data, onClick }) => {
   return (
     <div className="drill-list">
       {data.drills.map(item => 
-        <div id={item.id} onClick={onClick}>
+        <div key={item.id} id={item.id} onClick={onClick}>
           <header>
             <h2>{item.title}</h2>
           </header>
