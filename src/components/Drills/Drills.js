4f3fb2b896e3097071064d759be54ff109c6387b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 10:56:03 -0500

    Update Drills.js

--- src/components/Drills/Drills.js
+++ src/components/Drills/Drills.js
@@ -4,6 +4,7 @@ import Filters from '../Filters/Filters'
 import DrillList from '../DrillList/DrillList'
 import Modal from '../Modal/Modal'
 import { navigateTo } from '../Router/navigateTo'
+import { data } from '../../test_data.js'
 
 const Drills = () => {
   const [context, dispatch] = useContext(Context)
@@ -27,7 +28,7 @@ const Drills = () => {
       <h1>Drill Hub</h1>
       <button className="create-btn" onClick={handleCreateDrill}>Create</button>
       <Filters />
-      <DrillList />
+      <DrillList data={data} />
       <Modal className="modal" open={context.drill} onClose={handleCloseDrill}>
         {context.drill}
       </Modal>
