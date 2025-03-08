c254a7c2c800d125606b7e5c2582b705fe20238a
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 10:04:56 -0500

    Update test_data.js

--- src/test_data.js
+++ src/test_data.js
@@ -19,13 +19,22 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 2)).toDateString()
     },
     {
-      title: 'Checking Skills – 1 Up - 1 Down – Gap Drill',
-      description: 'Gap and angling drill. Defensemen must touch goal line and forward must touch blue line. Defenseman tries to clear puck and forward tries to score',
-      subdescription: 'Forward wants to create space by using speed and attack the net Defensemen want to read the forward and create a tight gap. As defender approaches he must read the forwards control of puck, speed etc. and now must either pressure and “jump” him or contain. Defenseman does not want to back in on goalie and create a screen. “Stick on puck and body on body”',
-      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg',
-      age: 'U11 - U13',
+      title: 'Checking Skills – Continuous Angling',
+      description: 'Continuous angle drill. Player shoots puck and then picks up a second puck and skates up the wall. Checking player angles and makes contact turning the puck over. That player shoots and picks up a second puck. Drill is continuous',
+      subdescription: 'Players angle the offensive player to the wall but taking away the back of the net with body position and stick. As players move up wall get “stick on puck” and rub them out. Stick on ice and play the puck first. Recover puck and attack the net.',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_continuous_angling.jpg',
+      age: 'U13 and Up',
       category: 'Checking Drill',
-      date: new Date(new Date().setDate(new Date().getDate() - 2)).toDateString()
+      date: new Date(new Date().setDate(new Date().getDate() - 3)).toDateString()
+    },
+    {
+      title: 'Checking Skills – Forecheck Progression',
+      description: 'Forechecker angles. Forward around pylon and dumps to either corner. Defenseman steps up, pivots and turns to get puck from the corner. Fore checker approaches “inside out” and forced defenseman “up and out”.',
+      subdescription: 'Players angle the offensive player to the wall but taking away the back of the net with body position and stick. As players move up wall get “stick on puck” and rub them out. Stick on ice and play the puck first. Recover the puck. Defensemen are encouraged to use good breakout tactics such as angle, shoulder checks etc.',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_forecheck_progression.jpg',
+      age: 'U7 - U9',
+      category: 'Checking Drill',
+      date: new Date(new Date().setDate(new Date().getDate() - 4)).toDateString()
     },
   ]
 }
