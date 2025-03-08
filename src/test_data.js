1b4cc12fd7dd461e26ddb17cfe01b4567105286c
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 10:23:06 -0500

    Update test_data.js

--- src/test_data.js
+++ src/test_data.js
@@ -1,6 +1,7 @@
 export const data = {
   drills: [
     {
+      id: 1,
       title: 'Goaltending - Alternating Nets',
       description: 'Place 2 nets as shown, facing each other but not in front of each other so they are offset. Inside posts can be aligned. Goaltender starts in one net gets 2 shots, then has to turn and butterfly slide to get in the other net and then face 2 shots again. Do the sequence twice then rest',
       subdescription: 'Goaltender has to be set, quick turn, then slide to other net. Shooters let goalie get in position before shooting.',
@@ -10,6 +11,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()
     },
     {
+      id: 2,
       title: 'Checking Skills – 1 Up - 1 Down – Gap Drill',
       description: 'Gap and angling drill. Defensemen must touch goal line and forward must touch blue line. Defenseman tries to clear puck and forward tries to score',
       subdescription: 'Forward wants to create space by using speed and attack the net Defensemen want to read the forward and create a tight gap. As defender approaches he must read the forwards control of puck, speed etc. and now must either pressure and “jump” him or contain. Defenseman does not want to back in on goalie and create a screen. “Stick on puck and body on body”',
@@ -19,6 +21,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 2)).toDateString()
     },
     {
+      id: 3,
       title: 'Checking Skills – Continuous Angling',
       description: 'Continuous angle drill. Player shoots puck and then picks up a second puck and skates up the wall. Checking player angles and makes contact turning the puck over. That player shoots and picks up a second puck. Drill is continuous',
       subdescription: 'Players angle the offensive player to the wall but taking away the back of the net with body position and stick. As players move up wall get “stick on puck” and rub them out. Stick on ice and play the puck first. Recover puck and attack the net.',
@@ -28,6 +31,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 3)).toDateString()
     },
     {
+      id: 4,
       title: 'Checking Skills – Forecheck Progression',
       description: 'Forechecker angles. Forward around pylon and dumps to either corner. Defenseman steps up, pivots and turns to get puck from the corner. Fore checker approaches “inside out” and forced defenseman “up and out”.',
       subdescription: 'Players angle the offensive player to the wall but taking away the back of the net with body position and stick. As players move up wall get “stick on puck” and rub them out. Stick on ice and play the puck first. Recover the puck. Defensemen are encouraged to use good breakout tactics such as angle, shoulder checks etc.',
@@ -37,6 +41,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 4)).toDateString()
     },
     {
+      id: 5,
       title: 'Checking Skills – Forecheck Progression – F1 – F2',
       description: 'Forechecker angles forward around pylon and dumps to either corner. Defenseman steps up, pivots and turns to get puck from the corner. Fore checker approaches “inside out” and forces defenseman “up and out”. Second forward reads the dump and shuts down the wall. Either he or F1 makes contact and the other player picks up the puck.',
       subdescription: 'Players angle the offensive player to the wall by taking away the back of the net with body position and stick. As players move up wall get “stick on puck” and rub them out. Stick on ice and play the puck first. Second forechecker reads the flow and reacts to the situation. Defensemen are encouraged to use good breakout tactics such as angle, shoulder checks etc.',
@@ -46,6 +51,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 5)).toDateString()
     },
     {
+      id: 6,
       title: 'Checking Skills – Long Angle',
       description: 'Gap and angling drill in neutral zone. Defenseman leaves goal line and forward from far blue line. Defenseman must close gap and defend and forward tries to score. Forward has advantage with time and space',
       subdescription: 'Forward wants to use space by using speed and attack the net Defenseman wants to read the forward and create a tight gap. As defender approaches he must read the forwards control of puck, speed etc. and now must either pressure and “jump” him or contain. Defenseman does not want to back in on goalie and create a screen. “Stick on puck and body on body”. Defenseman wants to angle and try to influence the path of the forward.',
@@ -55,6 +61,7 @@ export const data = {
       date: new Date(new Date().setDate(new Date().getDate() - 6)).toDateString()
     },
     {
+      id: 7,
       title: 'Checking Skills – Mirror Gap Control',
       description: 'Playing a delay. Forward attacks and delays by turning to the wall. Defenseman closes gap and plays the 1-1.',
       subdescription: 'When a defender identifies an attacking forward in the defensive zone, they will lock on and play that 1-1. Defender uses good tactics: stick on ice, “stick on puck, body on body”, close the gap, protect the house, stay on the defensive side of the puck.',
@@ -62,6 +69,26 @@ export const data = {
       age: 'U7 - U9',
       category: 'Checking Drill',
       date: new Date(new Date().setDate(new Date().getDate() - 7)).toDateString()
+    },
+    {
+      id: 8,
+      title: 'Defence – Retrieval – Attack F1',
+      description: 'Defence and forwards start at blue line. Coach dumps puck in end zone and defence has to retrieve it and skate up ice Once defence touches puck, forward leaves blue line and forechecks as F1. D should wheel net and attack F1 straight on to force F1 into gliding and not allowing F1 to “set the angle” and have to guess which way D is going to skate',
+      subdescription: 'Fake, shoulder check on retrieval, attack F1 straight on Skate up middle of ice Force F1 to guess.',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/defence_retreival_attack_F1.jpg',
+      age: 'U7 - U9',
+      category: 'Defence',
+      date: new Date(new Date().setDate(new Date().getDate() - 8)).toDateString()
+    },
+    {
+      id: 9,
+      title: '',
+      description: '',
+      subdescription: '',
+      image: '',
+      age: 'U7 - U9',
+      category: '',
+      date: new Date(new Date().setDate(new Date().getDate() - 9)).toDateString()
     },
   ]
 }
