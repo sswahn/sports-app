d015e8c4bc0e99554817912b318be6c51250a765
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 09:57:04 -0500

    Update test_data.js

--- src/test_data.js
+++ src/test_data.js
@@ -4,13 +4,28 @@ export const data = {
       title: 'Goaltending - Alternating Nets',
       description: 'Place 2 nets as shown, facing each other but not in front of each other so they are offset. Inside posts can be aligned. Goaltender starts in one net gets 2 shots, then has to turn and butterfly slide to get in the other net and then face 2 shots again. Do the sequence twice then rest',
       subdescription: 'Goaltender has to be set, quick turn, then slide to other net. Shooters let goalie get in position before shooting.',
-      image: '',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/goaltending_alternating_nets.jpg',
       age: 'U7 - U9',
       category: 'Goaltending Drill',
       date: new Date(new Date().setDate(new Date().getDate() - 1)).toDateString()
     },
     {
-      
-    }
+      title: 'Checking Skills – 1 Up - 1 Down – Gap Drill',
+      description: 'Gap and angling drill. Defensemen must touch goal line and forward must touch blue line. Defenseman tries to clear puck and forward tries to score',
+      subdescription: 'Forward wants to create space by using speed and attack the net Defensemen want to read the forward and create a tight gap. As defender approaches he must read the forwards control of puck, speed etc. and now must either pressure and “jump” him or contain. Defenseman does not want to back in on goalie and create a screen. “Stick on puck and body on body”',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg',
+      age: 'U11 - U13',
+      category: 'Checking Drill',
+      date: new Date(new Date().setDate(new Date().getDate() - 2)).toDateString()
+    },
+    {
+      title: 'Checking Skills – 1 Up - 1 Down – Gap Drill',
+      description: 'Gap and angling drill. Defensemen must touch goal line and forward must touch blue line. Defenseman tries to clear puck and forward tries to score',
+      subdescription: 'Forward wants to create space by using speed and attack the net Defensemen want to read the forward and create a tight gap. As defender approaches he must read the forwards control of puck, speed etc. and now must either pressure and “jump” him or contain. Defenseman does not want to back in on goalie and create a screen. “Stick on puck and body on body”',
+      image: 'https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/checking_skills_1_up_1_down_gap_drill.jpg',
+      age: 'U11 - U13',
+      category: 'Checking Drill',
+      date: new Date(new Date().setDate(new Date().getDate() - 2)).toDateString()
+    },
   ]
 }
