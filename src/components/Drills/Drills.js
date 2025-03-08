ab601c73a175709e9b7ef622c6b67c2430b866e1
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-08 12:45:53 -0500

    Update Drills.js

--- src/components/Drills/Drills.js
+++ src/components/Drills/Drills.js
@@ -59,7 +59,7 @@ const Drills = () => {
     <div className="drills">
       <h1>Drill Hub</h1>
       <button className="create-btn" onClick={handleCreateDrill}>Create</button>
-      <Filters />
+      <Filters handleAges={handleAges} handleCategories={handleCategories} handleSort={handleSort} handleCount={handleCount} handleSearch={handleSearch} />
       <DrillList data={data} onClick={handleDisplayDrill} />
       <Modal className="modal" open={context.drill} onClose={handleCloseDrill}>
        {state && <Drill data={state} />}
