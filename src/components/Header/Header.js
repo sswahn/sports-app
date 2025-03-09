4eeb698b1108c03b0cdbd857a000a4611929f3c9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-09 17:04:14 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,9 @@
 const Header = () => {
   return (
     <header className="header">
+      <a href="/sports-app">
+        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/flemingsbergsik_logo.png" alt="Flemingsberg Logo" />
+      </a>
     </header>
   )
 }
