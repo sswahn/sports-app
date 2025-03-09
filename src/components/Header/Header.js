456b7ece62ebd3494619b13482351e1d54c795e3
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-09 17:11:07 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,11 @@
 const Header = () => {
   return (
     <header className="header">
-      <a href="/sports-app">
-        <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/flemingsbergsik_logo.png" alt="Flemingsberg Logo" />
-      </a>
+      <div>
+        <a href="/sports-app">
+          <img src="https://raw.githubusercontent.com/sswahn/sports-app/refs/heads/main/public/images/flemingsbergsik_logo.png" alt="Flemingsberg Logo" />
+        </a>
+      </div>
     </header>
   )
 }
