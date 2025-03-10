cfa4754d163af9c828821d49abaacafc4186bf9d
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-10 11:49:31 -0400

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,17 +1,28 @@
+import { useState, useEffect } from 'react'
 import Select from '../Select/Select'
 import Search from '../Search/Search'
 import RefreshIcon from '../Icons/RefreshIcon/RefreshIcon'
 
 const Filters = ({ options, handleAges, handleCategories, handleSort, handleCount, handleSearch, handleRefresh }) => {
+  const [age, setAge] = useState('')
+  const [category, setCategory] = useState('')
+  const [sort, setSort] = useState('')
+
+  useEffect(() => {
+    setAge('')
+    setCategory('')
+    setSort('')
+  }, [options])
+  
   return (
     <div className="filters">
-      <select id="age" className="select" onChange={handleAges}>
+      <select id="age" className="select" value={age} onChange={handleAges}>
         {options.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
-      <select id="category" className="select" onChange={handleCategories}>
+      <select id="category" className="select" value={category} onChange={handleCategories}>
         {options.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
-      <select id="sort" className="select" onChange={handleSort}>
+      <select id="sort" className="select" value={sort} onChange={handleSort}>
         {options.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>                  
                                            
