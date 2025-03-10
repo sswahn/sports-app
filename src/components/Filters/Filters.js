4782b96269f422b9eeb7e916ee00dafabdb06196
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-10 11:21:17 -0400

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -6,25 +6,27 @@ const Filters = ({ drills, handleAges, handleCategories, handleSort, handleCount
 
   console.log('drills: ', drills)
   return (
-    <div className="filters">
-      <select id="age" className="select" onChange={handleAges}>
-        {drills.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
-      </select>
-      <select id="category" className="select" onChange={handleCategories}>
-        {drills.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
-      </select>
-      <select id="sort" className="select" onChange={handleSort}>
-        {drills.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
-      </select>                  
-                                           
-      <Select options={drills.count} onChange={handleCount} />
-      <Search className="search-drills" placeholder="Search Drills" onSubmit={handleSearch} />
-
-      <button className="refresh-btn" type="button" onClick={handleRefresh}>
-        <RefreshIcon />
-        <div className="tooltip" role="tooltip">Reset</div>
-      </button>
-    </div>
+    {drills.length && 
+      <div className="filters">
+        <select id="age" className="select" onChange={handleAges}>
+          {drills.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        </select>
+        <select id="category" className="select" onChange={handleCategories}>
+          {drills.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        </select>
+        <select id="sort" className="select" onChange={handleSort}>
+          {drills.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        </select>                  
+                                             
+        <Select options={drills.count} onChange={handleCount} />
+        <Search className="search-drills" placeholder="Search Drills" onSubmit={handleSearch} />
+  
+        <button className="refresh-btn" type="button" onClick={handleRefresh}>
+          <RefreshIcon />
+          <div className="tooltip" role="tooltip">Reset</div>
+        </button>
+      </div>
+    }
   )
 }
 
