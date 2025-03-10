0fc5db3925824e6bcd88caaa16ead5ec2d699daa
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-10 11:29:04 -0400

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -2,23 +2,20 @@ import Select from '../Select/Select'
 import Search from '../Search/Search'
 import RefreshIcon from '../Icons/RefreshIcon/RefreshIcon'
 
-const Filters = ({ drills, handleAges, handleCategories, handleSort, handleCount, handleSearch, handleRefresh }) => {
-
-  console.log('drills: ', drills)
-  
-  return drills.length && 
+const Filters = ({ config, handleAges, handleCategories, handleSort, handleCount, handleSearch, handleRefresh }) => {
+  return (
     <div className="filters">
       <select id="age" className="select" onChange={handleAges}>
-        {drills.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {config.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
       <select id="category" className="select" onChange={handleCategories}>
-        {drills.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {config.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
       <select id="sort" className="select" onChange={handleSort}>
-        {drills.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {config.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>                  
                                            
-      <Select options={drills.count} onChange={handleCount} />
+      <Select options={config.count} onChange={handleCount} />
       <Search className="search-drills" placeholder="Search Drills" onSubmit={handleSearch} />
 
       <button className="refresh-btn" type="button" onClick={handleRefresh}>
@@ -26,6 +23,7 @@ const Filters = ({ drills, handleAges, handleCategories, handleSort, handleCount
         <div className="tooltip" role="tooltip">Reset</div>
       </button>
     </div>
+  )
 }
 
 export default Filters
