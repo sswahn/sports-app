910bfbd1a40cde2c7c02e5c97e96df144381871f
S. Swahn <36422765+sswahn@users.noreply.github.com>
2025-03-10 11:13:44 -0400

    Update Filters.js

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,22 +1,21 @@
 import Select from '../Select/Select'
 import Search from '../Search/Search'
 import RefreshIcon from '../Icons/RefreshIcon/RefreshIcon'
-import { config } from '../../config'
 
-const Filters = ({ handleAges, handleCategories, handleSort, handleCount, handleSearch, handleRefresh }) => {
+const Filters = ({ drills, handleAges, handleCategories, handleSort, handleCount, handleSearch, handleRefresh }) => {
   return (
     <div className="filters">
       <select id="age" className="select" onChange={handleAges}>
-        {config.data.drills.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {drills.ages.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
       <select id="category" className="select" onChange={handleCategories}>
-        {config.data.drills.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {drills.categories.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>
       <select id="sort" className="select" onChange={handleSort}>
-        {config.data.drills.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
+        {drills.sort.map((item, index) => <option key={index} hidden={index === 0}>{item}</option>)}
       </select>                  
                                            
-      <Select options={config.data.drills.count} onChange={handleCount} />
+      <Select options={drills.count} onChange={handleCount} />
       <Search className="search-drills" placeholder="Search Drills" onSubmit={handleSearch} />
 
       <button className="refresh-btn" type="button" onClick={handleRefresh}>
